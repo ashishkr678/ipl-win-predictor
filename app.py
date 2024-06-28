283155bff0adb2d0de5aeecf6e7da28184258c53
Ashish Kumar <ashishkumat818@gmail.com>
2024-06-28 20:45:35 +0530

    a

--- app.py
+++ app.py
@@ -55,3 +55,5 @@ if st.button('Predict Probability'):
     win = result[0][1]
     st.header(batting_team + "- " + str(round(win*100)) + "%")
     st.header(bowling_team + "- " + str(round(loss*100)) + "%")
+
+st.header('Created By Ashish Kumar')
